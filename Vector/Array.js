794067c7d6e7acd8f2880ebf7c129216f5bd2705
GiancarloAparicio <zairo2542@gmail.com>
2020-08-14 09:50:56 -0500

    Create Array

---
+++ Vector/Array.js
@@ -0,0 +1,15 @@
+/**
+ *  Vectores
+ **/
+
+var array = [1, 2, 3];
+array[4]; //6
+
+// Insert to tail
+array.push(4); // [1, 2, 3, 4]
+
+// Insert/update values anywhere
+array[8] = 9; // [1, 2, 3, 4, null, 9]
+
+// Deleting last element from the array
+array.pop(); // [1, 2, 3, 4, null]
